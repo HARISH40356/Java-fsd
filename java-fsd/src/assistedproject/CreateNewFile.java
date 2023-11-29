1b5ccca3f7987a36d610b2bc6c0957484a1aca0a
HARISH40356 <harish22092000csk@gmail.com>
2023-11-29 23:06:52 +0530

    change

--- java-fsd/src/assistedproject/CreateNewFile.java
+++ java-fsd/src/assistedproject/CreateNewFile.java
@@ -1,10 +1,9 @@
 package assistedproject;
 
-import java.io.IOException; 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.FileWriter;
-import java.io.IOException;
+
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
